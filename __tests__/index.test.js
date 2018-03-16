05d8b7da4ce2d5300167cf8b2717849498b6bf8d
Steven Zeck <steven.zeck@gmail.com>
2018-03-16 14:51:19 -0500

    Add a few tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,98 @@
-// You can import your modules
-// const index = require('../index')
+// Requiring probot allows us to mock out a robot instance
+const {createRobot} = require('probot')
+const app = require('../index.js')
+const issueOpened = require('./fixtures/issueOpened')
+const issueReopened = require('./fixtures/issueReopened')
+const issueWithChecked = require('./fixtures/issueWithCheckboxesChecked')
+const issueUpdatedWithChecked = require('./fixtures/issueUpdatedWithCheckboxes')
 
-test('that we can run tests', () => {
-  // your real tests go here
-  expect(1 + 2 + 3).toBe(6)
+let robot
+// let issue
+let github
+
+beforeEach(() => {
+  robot = createRobot()
+  app(robot)
+  // issue = {
+  //   body: '- [ ] Task 1\n- [ ] Task 2\n- [ ] Task 3',
+  //   number: 100,
+  //   labels: [{
+  //     name: 'waiting-for-user-information',
+  //     color: 'f7c6c7'
+  //   }]
+  // };
+  github = {
+    repos: {
+      getContent: jest.fn().mockImplementation(() => Promise.resolve({
+        data: {content: Buffer.from(`labelName: waiting-for-user-information\nlabelColor: f7c6c7\ncommentText: Thanks for opening an issue on bot-testing.`).toString('base64')}
+      }))
+    },
+    issues: {
+      createLabel: jest.fn(),
+      removeLabel: jest.fn(),
+      createComment: jest.fn(),
+      addLabels: jest.fn(),
+      getLabel: jest.fn().mockImplementation(() => Promise.reject(new Error()))
+    }
+  }
+  robot.auth = () => Promise.resolve(github)
 })
 
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
+test('adds a label and comment to a newly opened issue without checkboxes filled', async () => {
+  await robot.receive(issueOpened)
+  expect(github.repos.getContent).toHaveBeenCalledWith({
+    owner: 'szeck87',
+    repo: 'bot-testing',
+    path: '.github/issuecomplete.yml'
+  })
+  expect(github.issues.addLabels).toHaveBeenCalled()
+  expect(github.issues.createComment).toHaveBeenCalled()
+})
+
+test('adds a label and comment to a reopened issue without checkboxes filled', async () => {
+  await robot.receive(issueReopened)
+  expect(github.repos.getContent).toHaveBeenCalledWith({
+    owner: 'szeck87',
+    repo: 'bot-testing',
+    path: '.github/issuecomplete.yml'
+  })
+  expect(github.issues.addLabels).toHaveBeenCalled()
+  expect(github.issues.createComment).toHaveBeenCalled()
+})
+
+test('does not add label or comment to opened issue with checkboxes filled', async () => {
+  await robot.receive(issueWithChecked)
+  expect(github.repos.getContent).toHaveBeenCalledWith({
+    owner: 'szeck87',
+    repo: 'bot-testing',
+    path: '.github/issuecomplete.yml'
+  })
+  expect(github.issues.addLabels).not.toHaveBeenCalled()
+  expect(github.issues.createComment).not.toHaveBeenCalled()
+})
+
+test('removes label to updated issue with checkboxes filled', async () => {
+  await robot.receive(issueUpdatedWithChecked)
+  expect(github.repos.getContent).toHaveBeenCalledWith({
+    owner: 'szeck87',
+    repo: 'bot-testing',
+    path: '.github/issuecomplete.yml'
+  })
+  expect(github.issues.removeLabel).toHaveBeenCalledWith({
+    owner: 'szeck87',
+    repo: 'bot-testing',
+    number: 26,
+    name: 'waiting-for-user-information'
+  })
+  expect(github.issues.addLabels).not.toHaveBeenCalled()
+  expect(github.issues.createComment).not.toHaveBeenCalled()
+})
+
+// test('errors when there is no config file', async () => {
+//   await robot.receive(issueOpened)
+//   expect(github.repos.getContent).toHaveBeenCalledWith({
+//     owner: 'szeck87',
+//     repo: 'bot-testing',
+//     path: '.github/issuecomplete.yml'
+//   })
+// })
